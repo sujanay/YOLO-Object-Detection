57ff956f9f308cc7c99a1e2cde68acfaeacefb8c
sujanay <sujanay@hotmail.com>
2018-06-10 14:32:37 -0400

    Create download_videos.sh

---
+++ nb_images/download_videos.sh
@@ -0,0 +1,7 @@
+!/usr/bin
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/pred_video.mp4
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/pred_video_compressed2.mp4
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/prediction_video_compressed.mp4
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video.mp4
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video_compressed.mp4
+curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video_compressed2.mp4
