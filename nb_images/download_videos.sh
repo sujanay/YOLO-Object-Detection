3187ce9213c9850d4a9dff7bebf2c11b26b1a3b9
sujanay <sujanay@hotmail.com>
2018-06-10 14:33:44 -0400

    Update download_videos.sh

--- nb_images/download_videos.sh
+++ nb_images/download_videos.sh
@@ -1,7 +1,9 @@
 !/usr/bin
+echo "Starting download..."
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/pred_video.mp4
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/pred_video_compressed2.mp4
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/prediction_video_compressed.mp4
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video.mp4
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video_compressed.mp4
 curl -O https://s3.amazonaws.com/yolo-implementation-videos/road_video_compressed2.mp4
+echo "done!"
